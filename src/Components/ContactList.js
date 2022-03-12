61994babc0f469a9533e5a8705ddfc72d753d1b1
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-3.fritz.box>
2022-03-12 17:08:56 +0100

    fifth

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -2,7 +2,7 @@ const ContactItem = (props) => {
     return (
         <article id="contactItem">
             <img src={props.pictureUrl}
-                onError={(e) => (e.target.onerror = null, e.target.src = "https://images.unsplash.com/photo-1609743522653-52354461eb27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")} alt="photo" />
+                onError={(e) => (e.target.onerror = null, e.target.src = "https://images.unsplash.com/photo-1609743522653-52354461eb27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")} alt="" />
             <h6>{props.name}</h6>
             <h6>{props.popularity.toFixed(2)}</h6>
             <input className="delete-btn btn" type="button" value="cancel" onClick={() => props.deleteContact(props.popularity)} />
