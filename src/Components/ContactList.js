3991bb6acef56ebb0c75617e9cbc84f3d191842a
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-3.fritz.box>
2022-03-12 16:52:06 +0100

    thrd com

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,7 +1,6 @@
 const ContactItem = (props) => {
     return (
         <article id="contactItem">
-            {/* <img src={props.pictureUrl} alt="photp" /> */}
             <img src={props.pictureUrl}
                 onError={(e) => (e.target.onerror = null, e.target.src = "https://images.unsplash.com/photo-1609743522653-52354461eb27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")} alt="photo" />
             <h6>{props.name}</h6>
